7be75d3a6aa3309270bf5bdb898105377671ad29
gitbarnabedikartola <gitbarnabedikartola@users.noreply.github.com>
2023-05-06 23:12:57 +0000

    new version

---
+++ compilation/build/src/CMakeFiles/corectrl.dir/qrc_resources.cpp.o.d
@@ -0,0 +1,3 @@
+src/CMakeFiles/corectrl.dir/qrc_resources.cpp.o: \
+ /home/runner/work/Jibreel-compiler/Jibreel-compiler/corectrl/build/src/qrc_resources.cpp \
+ /usr/include/stdc-predef.h
---
+++ compilation/build/src/helper/CMakeFiles/corectrl_helperkiller.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for corectrl_helperkiller.
+# This may be replaced when dependencies are built.
